cbedfc198d0cf20d08da414973d10bf44db0d349
Marwan <marwaneg@aucegypt.edu>
2023-11-14 15:52:43 +0200

    Create cart items table

--- database/migrations/2023_11_14_131846_create_cart_items_table.php
+++ database/migrations/2023_11_14_131846_create_cart_items_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignIdFor(\App\Models\User::class, 'user_id');
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->integer('quantity');
         });
     }
 
