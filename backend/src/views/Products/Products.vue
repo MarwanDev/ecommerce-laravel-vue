1ed87785967ee3cc7b2fb1f897b432d0f1c0c365
Marwan <marwaneg@aucegypt.edu>
2023-11-16 00:26:56 +0200

    Create components and not found page

--- backend/src/views/Products/Products.vue
+++ backend/src/views/Products/Products.vue
@@ -1,25 +1,9 @@
 <template>
-    <div class="flex items-center justify-between mb-3">
-      <h1 class="text-3xl font-semibold">Products</h1>
-      <router-link :to="{name: 'app.products.create'}"
-                   class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        Add new Product
-      </router-link>
-    </div>
-    <!-- <ProductsTable/> -->
-  </template>
+  <h1>Products</h1>
+</template>
   
-  <script setup>
-  import {computed} from "vue";
-  import store from "../../store";
-//   import ProductsTable from "./ProductsTable.vue";
+<script setup>
+
+</script>
   
-  const products = computed(() => store.state.products);
-  
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+<style scoped></style>
